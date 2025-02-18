9655ad09e1b5804e116963927d2f1888a1cc0f94
Nilesh Lachheta <nileshblachheta@gmail.com>
2025-02-18 23:01:33 +0530

    configure google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactGA from 'react-ga4'
+
+const TRACKING_ID = 'G-CSSG14Q2KG';
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType : "pageview",
+      page : window.location.pathname + window.location.search,
+      title : "Home Page"
+    });
+  },[])
 
   return (
     <>
@@ -21,9 +33,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>This is a Vite + React app with a counter button. for google analytics implementation</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
